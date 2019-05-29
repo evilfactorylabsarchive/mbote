0fd06cfd6737fcfc69fb7f5eb23437fedadbb325
ri7nz <ri7nz.labs@gmail.com>
2019-05-30 05:25:02 +0700

    init: basic configuration

---
+++ src/index.test.js
@@ -0,0 +1,11 @@
+/**
+ * - Fetch data from FEED Url 
+ * - should have data
+ * - post to webhook if have a new data 
+ */
+
+describe('HELLO', () => {
+    test('Fetch Data from FEED URL', () => {
+    
+    })
+})
