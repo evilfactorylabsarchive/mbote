79c7457b00b749ae6dcde6119653a9f45f77f757
Fariz Rizaldy <farizrizaldy@outlook.co.id>
2019-06-15 23:27:27 +0700

    style: format to preffered .prettierrc

--- src/index.js
+++ src/index.js
@@ -1,61 +1,61 @@
-const feedSub = require("feedsub");
-const fetch = require("node-fetch");
-const rssList = require("./member.json").rss;
-const has = Function.call.bind(Object.hasOwnProperty);
-const webhook = process.env.DISCORD_WEBHOOK;
+const feedSub = require('feedsub')
+const fetch = require('node-fetch')
+const rssList = require('./member.json').rss
+const has = Function.call.bind(Object.hasOwnProperty)
+const webhook = process.env.DISCORD_WEBHOOK
 
 const feedConfig = {
   interval: 0.5,
   emitOnStart: true,
   autoStart: true
-};
+}
 
 const getByProp = (obj, prop) =>
-  typeof prop === "string"
-    ? prop.split(".").reduce((acc, cur, idx) => acc[cur], obj)
-    : obj;
+  typeof prop === 'string'
+    ? prop.split('.').reduce((acc, cur, idx) => acc[cur], obj)
+    : obj
 
 const handleItem = (item, format) => {
-  const formatter = format.split("|");
-  const normalize = ["title", "link"];
+  const formatter = format.split('|')
+  const normalize = ['title', 'link']
   const data = formatter.reduce((acc, cur, idx) => {
     acc[normalize[idx]] = has(item, cur)
       ? item[cur]
-      : getByProp(item, cur) || "New Post!!!!";
-    return acc;
-  }, {});
+      : getByProp(item, cur) || 'New Post!!!!'
+    return acc
+  }, {})
 
   setTimeout(fetch, Math.random() * (5000 - 2000) + 2000, webhook, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "content-type": "application/json"
+      'content-type': 'application/json'
     },
     body: JSON.stringify({
       content: `${data.title} - ${data.link}`
     })
-  });
-};
+  })
+}
 
 setInterval(
   () =>
     rssList.forEach(({ rss, format }) => {
-      const reader = new feedSub(rss, feedConfig);
-      reader.on("item", item => handleItem(item, format));
+      const reader = new feedSub(rss, feedConfig)
+      reader.on('item', item => handleItem(item, format))
     }),
   2000
-);
-if (process.platform === "win32") {
-  var rl = require("readline").createInterface({
+)
+if (process.platform === 'win32') {
+  var rl = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
-  });
+  })
 
-  rl.on("SIGINT", function() {
-    process.emit("SIGINT");
-  });
+  rl.on('SIGINT', function() {
+    process.emit('SIGINT')
+  })
 }
 
-process.on("SIGINT", function() {
+process.on('SIGINT', function() {
   //graceful shutdown
-  process.exit();
-});
+  process.exit()
+})
